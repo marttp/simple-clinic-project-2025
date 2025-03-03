2ac9cc1aed01328212479fe226a693219e886978
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-03-04 07:48:44 +0900

    Integrate Pharmacy service

--- clinic/src/main/kotlin/dev/tpcoder/clinic/controller/InternalController.kt
+++ clinic/src/main/kotlin/dev/tpcoder/clinic/controller/InternalController.kt
@@ -2,21 +2,27 @@ package dev.tpcoder.clinic.controller
 
 import dev.tpcoder.clinic.model.Doctor
 import dev.tpcoder.clinic.service.DoctorService
+import dev.tpcoder.clinic.service.external.PharmacyService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/v1/doctors")
-class DoctorController(
+@RequestMapping("/v1/internal")
+class InternalController(
     private val doctorService: DoctorService,
+    private val pharmacyService: PharmacyService
 ) {
 
-    @GetMapping
+    @GetMapping("/doctors")
     fun getDoctors(): List<Doctor> =
         doctorService.getDoctors()
 
-    @PostMapping
+    @PostMapping("/doctors")
     @ResponseStatus(HttpStatus.CREATED)
     fun addDoctor(@RequestBody doctor: Doctor) =
         doctorService.addDoctor(doctor)
+
+    @GetMapping("/medicines")
+    fun getListOfAvailableMedicines() =
+        pharmacyService.getMedicines()
 }
